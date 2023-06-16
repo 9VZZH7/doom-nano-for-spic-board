6c53df63c4982a8b44f62ab6b6679f398821fb2c
Jannik Hausladen <jannik.hausladen@fau.de>
2023-06-16 12:15:21 +0200

    [MOD] installer can erase, and saves power

--- installer.c
+++ installer.c
@@ -1,14 +1,36 @@
 #include "level.h"
 
 #include <display.h>
-#include <button.h>
-#include <timer.h>
-#include <7seg.h>
 
+#include <avr/sleep.h>
 #include <avr/eeprom.h>
 #include <avr/interrupt.h>
 
+static volatile uint8_t install = 0;
+static volatile uint8_t erase = 0;
+
+ISR(INT0_vect){
+	install = 1;
+}
+
+ISR(INT1_vect){
+	erase = 1;
+}
+
 static void init(void){
+	// configure button 0
+	DDRD &= ~(1 << PD2);
+	PORTD |= (1 << PD2);
+	EICRA &= ~(1 << ISC00);
+	EICRA |= (1 << ISC01);
+	EIMSK |= (1 << INT0);
+	// config button 1
+	DDRD &= ~(1 << PD3);
+	PORTD |= (1 << PD3);
+	EICRA &= ~(1 << ISC10);
+	EICRA |= (1 << ISC11);
+	EIMSK |= (1 << INT1);
+	// config display
 	sb_display_enable();
 	sb_display_fillScreen(NULL);
 }
@@ -17,6 +39,14 @@ static void start_installation(void){
 	for(uint16_t i = 0; i < 1024; i++){
 		eeprom_busy_wait();
 		eeprom_update_byte(i, *(sto_level_1 + i));
+	}
+	return;
+}
+
+static void start_erase(void){
+	for(uint16_t i = 0; i < 1024; i++){
+		eeprom_busy_wait();
+		eeprom_update_byte(i, 0xFF); 
 	}
 	return;
 }
@@ -33,11 +63,28 @@ void main(void){
 	sb_display_showStringSmall(2, 0, str2);
 	sb_display_showStringSmall(3, 0, str3);
 	sb_display_showStringSmall(4, 0, str4);
-	
-	while(sb_button_getState(BUTTON0) == RELEASED){}
-	sb_display_showString(7, 5, "Starting...");
-	start_installation();
 
-	sb_display_showString(7, 3, "Finished! GLHF");
-	while(1);
+	sleep_enable();
+	cli();
+	while(install == 0 && erase == 0){
+		sei();
+		sleep_cpu();
+		cli();
+	}
+	sei();
+	sleep_disable();
+	
+	if(install){
+		sb_display_showString(7, 5, "Starting...");
+		start_installation();
+		sb_display_showString(7, 3, "Finished! GLHF");
+		while(1);
+	}
+	if(erase){
+		sb_display_showString(7, 5, "Starting...");
+		start_erase();
+		sb_display_showString(7, 3, "ERASED EEPROM");
+		while(1);
+	}
+
 }
